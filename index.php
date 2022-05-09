bcef3b6de0eb2ee9cce59749a6624ad7800eecc7
massej6it <jarnonly@gmail.com>
2022-05-09 15:25:16 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,30 @@
+﻿<!DOCTYPE html>
+
+<html lang="en">
+<head>
+    <meta charset="utf-8" />
+    <title>BEDrugs</title>
+</head>
+<body>
+    <!-- Start Main Content-->
+    <div id="Main">
+        <!-- Start Header -->
+        <div id="Header">
+
+        </div>
+        <!--Start Content-->
+        <div id="Content">
+
+        </div>
+        <!--Start footer-->
+        <div id="ContentFooter">
+
+        </div>
+    </div>
+    <!--End Main Content-->
+
+    <!--Start Footer -->
+    <div id="Footer">
+    </div>
+</body>
+</html>
